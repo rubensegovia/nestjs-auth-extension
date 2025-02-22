ac8cc5ebbd14975553a19bbedef9cc7b59779b77
Rubén Carrasco <rubencf@ext.inditex.com>
2025-02-22 10:24:09 +0100

    feat: auth with jwt and refresh token rotation

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { Auth } from './decorators/auth.decorator';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { AuthType } from './enums/auth-type.enum';
+
+@Auth(AuthType.None)
+@Controller('authentication')
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @HttpCode(200)
+  @Post('sign-in')
+  signIn(
+    // @Res({ passthrough: true }) response: Response, // 👈 from express
+    @Body() signInDto: SignInDto,
+  ) {
+    // Secure "HTTP only cookies" response
+    // const accessToken = await this.authService.signIn(signInDto);
+    // response.cookie('accessToken', accessToken, {
+    //   secure: true,
+    //   httpOnly: true,
+    //   sameSite: true,
+    // });
+    return this.authService.signIn(signInDto);
+  }
+
+  @HttpCode(HttpStatus.OK) // changed since the default is 201
+  @Post('refresh-tokens')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
+  }
+}
